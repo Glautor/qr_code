e0403e23eb222c04e4e4ad0a28f606e4ecdd9c46
marcoslunciel <marcoslunciel@hotmail.com>
2018-05-30 13:31:37 -0300

    vaiquevai

---
+++ app/src/main/java/com/example/usuario/qrcode/check.java
@@ -0,0 +1,13 @@
+package com.example.usuario.qrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+    }
+}
